72e71279ce350d10dcc8bc2e1fd2209e877c3494
Hugo Vasconcelos <hugo.silva08@hotmail.com>
2022-05-26 22:15:26 -0300

    Finalizacao do Teste 2

--- Teste2/teste2.py
+++ Teste2/teste2.py
@@ -1,9 +1,28 @@
+#Transformacao de Dados
 import tabula
+import pandas as pd
+from zipfile import ZipFile
 
-file = "Anexo_I.pdf"
+# Nomeacao dos arquivos utilizados
+fileRead = "Anexo_I.pdf"
+fileWrite = "Rol_Proced.csv"
+zipFile = "Teste_HugoSV.zip"
 
-table = tabula.read_pdf(file, pages = "all")
+#Leitura das tabelas do arquivo PDF
+tables  = tabula.read_pdf(fileRead, pages = "all")
 
-print (table)
+print(len(tables))
 
+#Bonus, traduz o conteudo das colunas selecionadas com base na legenda do rodape
+for i in range(0, len(tables)):
+    df = tables[i].replace({'OD':{'OD':'Seg. Odontológica'},'AMB':{'AMB': 'Seg. Ambulatorial'}})
+    tables[i] = df
 
+#Concatena a lista de dataframes (210 dataframes) em um unico dataframe
+table = pd.concat(tables, ignore_index=True)
+
+#Transforma o dataframe em um arquivo CSV
+table.to_csv(fileWrite)
+#Cria um arquivo .zip com o csv criado acima
+zipObj = ZipFile(zipFile, 'w')
+zipObj.write(fileWrite)
